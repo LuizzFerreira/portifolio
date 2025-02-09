432f69259e08b85fdf9d820093443e4951df2d44
LuizzFerreira <160446147+LuizzFerreira@users.noreply.github.com>
2025-02-09 16:37:25 -0300

    Update splide.js

--- assets/js/splide.js
+++ assets/js/splide.js
@@ -12,3 +12,21 @@ document.addEventListener("DOMContentLoaded", function () { //executa o js depoi
       splide.mount();
     }
   });
+
+  document.addEventListener("DOMContentLoaded", function () { //executa o js depois de todo o HTML carregado
+    for (var x of document.querySelectorAll(".splide.recommendations-container")) {
+      var splide = new Splide(x, {
+        perPage: 1,
+        type: 'rewind',
+        pagination: true,
+        arrows: true,
+        autoplay: true,
+			  gap: 22,
+			  interval: 7000,
+        breakpoints: {
+          769: { arrows: false },
+      },
+      });
+      splide.mount();
+    }
+  });
